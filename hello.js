f303ceb51cdb40ab7182416d53798e26b9684edb
JorgeIsur <ramirezisur@gmail.com>
2021-10-25 19:39:20 -0500

    ejemplo 1

---
+++ hello.js
@@ -0,0 +1,7 @@
+var msg = 'Hello world';
+var num = 2;
+var num2 = 3;
+var resultado = num+num2;
+console.log("La operación suma entre "+num+" "+"y "+num2);
+console.log("El resultado es "+resultado);
+console.log(msg);
