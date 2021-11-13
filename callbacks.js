7b31a3fe80b346190f8ebb2b2db089e703cf18aa
JorgeIsur <ramirezisur@gmail.com>
2021-11-12 19:05:57 -0600

    algo q no entendi xd

---
+++ callbacks.js
@@ -0,0 +1,11 @@
+function saludar(nombre) { 
+    console.log('Hola ' + nombre); 
+} 
+function pedirNombre(callback){ 
+    console.log("Escribe tu nombre: "); 
+    process.stdin.on('data', function (line) { 
+    callback(line); 
+    process.exit(); 
+    }); 
+} 
+pedirNombre(saludar); 
