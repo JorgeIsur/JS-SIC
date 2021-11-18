bc3328aef66ee7cf8455eb8aad93521b490bae74
JorgeIsur <ramirezisur@gmail.com>
2021-11-17 19:08:23 -0600

    servidor

---
+++ servidorWeb/index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const host = 'localhost';
+const port = 8000;
+
+const requestListener =  function(req,res){
+    res.setHeader("Content-type","text/html");
+    res.end('<html><body><h1>This is HTML</h1></body></html>');
+};
+const server = http.createServer(requestListener);
+server.listen(port,host,()=>{
+    console.log('Server is runnin on http://'+host+":"+port);
+});
