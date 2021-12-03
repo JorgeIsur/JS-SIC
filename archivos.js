635563ce9d1ab024d93269954ce331e956aee455
JorgeIsur <ramirezisur@gmail.com>
2021-12-02 18:55:16 -0600

    archivos en js

--- archivos.js
+++ archivos.js
@@ -9,5 +9,8 @@ fs.writeFile('Archivo2.txt',"Archivo creado de manera asíncronica.",(err)=>{
   }
 });
 let informacion = fs.readFileSync('Archivo.txt','utf8');
+let informacion2 =  fs.readFileSync('Archivo2.txt','utf8');
 console.log(informacion);
+console.log(informacion2)
 fs.unlinkSync('Archivo.txt');
+fs.unlinkSync('Archivo2.txt');
